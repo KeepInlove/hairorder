a8547bf375ad9509f631bebea65c2fcea28fc406
guo <904004593@qq.com>
2021-12-08 19:50:29 +0800

    1.3GitHub修改名字测试

---
+++ src/main/java/com/gxy/hairorder/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.gxy.hairorder.repository;
+
+import com.gxy.hairorder.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author GUO
+ * @Classname OrderRepsitory
+ * @Description TODO
+ * @Date 2021/12/8 19:48
+ */
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
